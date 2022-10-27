97a45530685dc9ef88e083ea6ac48dfc5c81b155
David Christian Jones <david@dcj.codes>
2022-10-27 10:57:03 -0400

    fucking around with yargs and fs

--- node/core/notes/index.js
+++ node/core/notes/index.js
@@ -2,14 +2,63 @@ const notes = require("./notes");
 const chalk = require("chalk");
 const yargs = require("yargs");
 
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "title of the note",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: "body of the note",
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler({ title, body }) {
+    notes.addNote(title, body);
+  },
+})
 
-const args = yargs.argv
+yargs.command({
+  command: "remove",
+  describe: "Removes a note",
+  builder: {
+    title: {
+      describe: "title of the note",
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler({ title }) {
+    notes.removeNote(title);
+  },
+})
 
-const isAdd = args.add;
-const message = args.message
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler() {
+    // notes.listNotes();
+  },
+});
 
-if (isAdd) {
-  notes.addNote("test", message);
-} else {
-//   notes.removeNote("test");
-}
+yargs.command({
+  command: "read",
+  describe: "Reads a note",
+  builder: {
+    title: {
+      describe: "title of the note",
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler({ title }) {
+    notes.readNote(title);
+  },
+});
+
+
+yargs.parse()
--- node/core/notes/notes.js
+++ node/core/notes/notes.js
@@ -4,17 +4,25 @@ const createNote = (name, initialData) => {
   return fs.writeFileSync(`${name}.txt`, initialData);
 };
 
-const getNote = (name) => {
+const readNote = (name) => {
+
+  console.log(name);
   const file = fs.readFileSync(`${name}.txt`, { encoding: "utf8" });
 
   console.log(file);
 };
 
 const addNote = (name, note) => {
-    console.log(note);
+  console.log("ADD: ", note);
   fs.appendFile(`${name}.txt`, note, () => {
     console.log("Note updated");
   });
 };
 
-module.exports = { createNote, getNote, addNote };
+const removeNote = (name) => {
+  fs.unlink(`${name}.txt`, () => {
+    console.log("Note removed");
+  });
+};
+
+module.exports = { createNote, readNote, addNote, removeNote };
