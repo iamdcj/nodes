775cadcedbc004f2b5d6693fb5322a81ea002d06
David Christian Jones <david@dcj.codes>
2022-10-31 11:04:39 -0400

    weather app beginnings

--- node/core/weather-app/index.js
+++ node/core/weather-app/index.js
@@ -1,4 +1,21 @@
-require('dotenv').config();
+const request = require("postman-request");
 
-console.log(process.env.WEATHER_STACK_API_KEY);
+require("dotenv").config();
 
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=New%20ork`;
+
+const returnWeather = () => {
+  return request({ url, json: true }, (error, response, { current }) => {
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    const { temperature, feelslike } = current;
+
+    console.log(
+      `It is currently ${temperature} degrees out, but it feels like ${feelslike} degrees`
+    );
+  });
+};
+
+returnWeather();
