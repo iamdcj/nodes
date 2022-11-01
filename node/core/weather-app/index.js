2ea35025980a497292d7cda61075cf4df97a9e57
David Christian Jones <david@dcj.codes>
2022-11-01 09:48:27 -0400

    NODE | weather app start

--- node/core/weather-app/index.js
+++ node/core/weather-app/index.js
@@ -2,7 +2,7 @@ const request = require("postman-request");
 
 require("dotenv").config();
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=New%20ork`;
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=New%20York&units=f`;
 
 const returnWeather = () => {
   return request({ url, json: true }, (error, response, { current }) => {
@@ -10,8 +10,9 @@ const returnWeather = () => {
       throw new Error(error.message);
     }
 
-    const { temperature, feelslike } = current;
+    const { temperature, feelslike, ...rest } = current;
 
+    console.log(rest);
     console.log(
       `It is currently ${temperature} degrees out, but it feels like ${feelslike} degrees`
     );
