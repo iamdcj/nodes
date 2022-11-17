dc6e9d056bc987fe9dfe6dfec871ab5e3a236a5a
David Christian Jones <david@dcj.codes>
2022-11-17 13:11:52 -0500

    core http request

---
+++ playground/http-requests.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const returnWeather = () => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=d7574e0584ab81cfc7e37919e71a6161&query=40.652601,-73.949721&units=f";
+
+  const request = http.request(url, (response) => {
+    let data = "";
+
+
+    response.on("data", (chunk) => {
+      data += chunk.toString();
+    });
+
+    response.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.on("error", (error) => {
+    console.log(error);
+  });
+
+  request.end();
+};
+
+returnWeather();
