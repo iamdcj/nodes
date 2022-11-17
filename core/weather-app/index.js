e10705d8aa9e6765ac2566e85bcdc7e962a8c6ce
David Christian Jones <david@dcj.codes>
2022-11-17 11:07:42 -0500

    chain calls

--- core/weather-app/index.js
+++ core/weather-app/index.js
@@ -2,21 +2,34 @@ const request = require("postman-request");
 
 require("dotenv").config();
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=New%20York&units=f`;
+const returnWeather = (coords) => {
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=${coords}&units=f`;
+  
 
-const returnWeather = () => {
   return request({ url, json: true }, (error, response, { current }) => {
     if (error) {
-      throw new Error(error.message);
+      throw new Error(error?.message || "Something went wrong");
     }
 
     const { temperature, feelslike, ...rest } = current;
 
-    console.log(rest);
+
     console.log(
       `It is currently ${temperature} degrees out, but it feels like ${feelslike} degrees`
     );
   });
 };
 
-returnWeather();
+const returnCoords = (address = "manhattan") => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${process.env.MAPBOX_TOKEN}&limit=1`;
+
+  return request({ url, json: true }, (error, response, { features }) => {
+    if (error) {
+      throw new Error(error?.message || "Something went wrong");
+    }
+
+    returnWeather(features[0].geometry.coordinates);
+  });
+};
+
+returnCoords();
