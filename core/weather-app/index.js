98e790f4f46d4d12be88a49a052d8c48aa2df89a
David Christian Jones <david@dcj.codes>
2022-11-17 11:29:09 -0500

    defensive code

--- core/weather-app/index.js
+++ core/weather-app/index.js
@@ -4,14 +4,15 @@ require("dotenv").config();
 
 const returnWeather = (city, [lon = -73.959894, lat = 40.789624]) => {
   const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_API_KEY}&query=${lat},${lon}&units=f`;
-
-  console.log(url);
+  
   return request({ url, json: true }, (error, response, data) => {
-    if (error) {
-      throw new Error(error?.message || "Something went wrong");
+
+    if (error || response.body.error) {
+      throw new Error(
+        error?.message || response.body?.error?.info || "Something went wrong"
+      );
     }
 
-    console.log();
     const { temperature, feelslike, ...rest } = data.current;
 
     console.log(
@@ -24,8 +25,10 @@ const returnCoords = (address = "brooklyn") => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${process.env.MAPBOX_TOKEN}&limit=1`;
 
   return request({ url, json: true }, (error, response, { features }) => {
-    if (error) {
-      throw new Error(error?.message || "Something went wrong");
+    if (error || response.body.error) {
+      throw new Error(
+        error?.message || response.body?.error?.info || "Something went wrong"
+      );
     }
 
     returnWeather(features[0].text, features[0].geometry.coordinates);
