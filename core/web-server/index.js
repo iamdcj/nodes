15473d115ae26c1b0b024c8d96b28dd8be19b235
David Christian Jones <david@dcj.codes>
2022-11-17 16:20:03 -0500

    add additional routes

--- core/web-server/index.js
+++ core/web-server/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.get("", ({ req, res }) => {
+  res.send(
+    "Welcome."
+  );
+});
+
 app.get("/health-check", ({ req, res }) => {
   res.send(
     "In Heaven Everything is fine. You got your good things. And I've got mine."
   );
+});
+
+app.get("/help", ({ req, res }) => {
+  res.send("What is the problem?");
 });
 
 app.listen(1919, () => {
