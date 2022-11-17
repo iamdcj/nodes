d0f4ce9add90eeeabe841a208568746a50c388b6
David Christian Jones <david@dcj.codes>
2022-11-17 17:07:00 -0500

    switch weather application structure

--- core/weather-app/index.js
+++ core/weather-app/index.js
@@ -1,14 +1,29 @@
-const { geocode } = require("./geocode");
-const { returnWeather } = require("./weather");
+const express = require("express");
 
-geocode("boston", (error, { text, coordinates }) => {
-  try {
-    if (error) {
-      throw new Error(error);
-    }
+const app = express();
 
-    returnWeather(text, coordinates);
-  } catch (error) {
-    console.log(error.message);
-  }
+app.get("", ({ res }) => {
+  res.send("Welcome.");
+});
+
+app.get("/about", ({ res }) => {
+  res.send("<h1>About Page</h1>");
+});
+
+app.get("/weather", ({ res }) => {
+  res.json({ location: "New York", forecast: "Shite" });
+});
+
+app.get("/health-check", ({ res }) => {
+  res.send(
+    "In Heaven Everything is fine. You got your good things. And I've got mine."
+  );
+});
+
+app.get("/help", ({ res }) => {
+  res.send("What's up?");
+});
+
+app.listen(1919, () => {
+  console.log("Coming to you from 1919");
 });
